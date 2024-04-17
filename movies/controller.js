c4cacc1e9fbe72803478c47f3ef04c96e6076ca5
AndreevSV <andreevsv@gmail.com>
2024-04-17 19:40:00 +0300

    Controllers and servises

--- movies/controller.js
+++ movies/controller.js
@@ -8,6 +8,50 @@ export const getMovies = async (req, res) => {
   return res.json(movies);
 };
 
-export default {
-  getMovies,
-};
+
+export const getMovieById = async (req, res) => {
+    const { id } = req.params;
+    const movie = await MovieService.getMovieById(id);
+    if (!movie) {
+      return res.status(404).json({ error: 'Movie not found' });
+    }
+    return res.json(movie);
+  };
+  
+  export const getMoviesByActorId = async (req, res) => {
+    const { actorId } = req.params;
+    const movies = await MovieService.getMoviesByActorId(actorId);
+    if (!movies.length) {
+      console.log('Movies not found for actorId:', actorId);
+    }
+    return res.json(movies);
+  };
+  
+  export const createMovie = async (req, res) => {
+    const movie = req.body;
+    const newMovie = await MovieService.createMovie(movie);
+    logger.info(`Movie created: ${JSON.stringify(newMovie)} req: ${req.uuid}`);
+    return res.status(201).json(newMovie);
+  };
+  
+  export const updateMovie = async (req, res) => {
+    const movie = req.body;
+    const id = req.params.id;
+    const updatedMovie = await MovieService.updateMovie(id, movie);
+    return res.status(201).json(updatedMovie);
+  };
+  
+  export const deleteMovie = async (req, res) => {
+    const { id } = req.params;
+    await MovieService.deleteMovie(id);
+    return res.status(204).end();
+  };
+  
+  export default {
+    getMovies,
+    getMovieById,
+    getMoviesByActorId,
+    createMovie,
+    updateMovie,
+    deleteMovie,
+  };
--- movies/service.js
+++ movies/service.js
@@ -9,7 +9,58 @@ const getMovies = async () => {
   }
 };
 
+export const getMovieById = async (id) => {
+    try {
+      return await MovieModel.findById(id);
+    } catch (error) {
+      logger.error(error);
+      return null;
+    }
+  };
+  
 
-export {
-  getMovies
-};
+  export const getMoviesByActorId = async (actorId) => {
+    try {
+      return await MovieModel.find({ actorId });
+    } catch (error) {
+      logger.error(error);
+      return null;
+    }
+  };
+  
+ 
+  export const createMovie = async (Movie) => {
+    try {
+      const newMovie = new MovieModel(Movie);
+      return await newMovie.save();
+    } catch (error) {
+      logger.error(error);
+      return null;
+    }
+  };
+  
+  export const updateMovie = async (id, Movie) => {
+    try {
+      return await MovieModel.findByIdAndUpdate(id, Movie, { new: true });
+    } catch (error) {
+      logger.error(error);
+      return null;
+    }
+  };
+  
+  export const deleteMovie = async (id) => {
+    try {
+      await MovieModel.findByIdAndDelete(id);
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+  
+  export default {
+    getMovies,
+    getMovieById,
+    getMoviesByActorId,
+    createMovie,
+    updateMovie,
+    deleteMovie,
+  };
