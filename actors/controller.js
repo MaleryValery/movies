145315ea4ddc10b683f4261e16beb2808c5abc76
Valeriia Melnikov <73602864+MaleryValery@users.noreply.github.com>
2024-04-19 01:31:14 +0300

    Update controller.js remove req.uuid

--- actors/controller.js
+++ actors/controller.js
@@ -34,7 +34,7 @@ export const getActorById = async (req, res) => {
 export const createActor = async (req, res) => {
   const actor = req.body;
   const newActor = await actorsService.addActor(actor);
-  logger.info(`Actor created: ${JSON.stringify(newActor)} req: ${req.uuid}`);
+  logger.info(`Actor created: ${JSON.stringify(newActor)});
   return res.status(201).json(newActor);
 };
 
