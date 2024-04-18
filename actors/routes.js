1a812a8f06e8d05e3177afa6605f76d6ceca7257
ilyanosovsky <imnosovsky@gmail.com>
2024-04-18 20:22:36 +0300

    get all and get by id fixed

--- actors/routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import actorController from './controller.js';
-
-const actorsRouter = express.Router();
-
-actorsRouter.get('/', actorController.getActors);
-
-export default actorsRouter;
--- config/index.js
+++
@@ -1,2 +0,0 @@
-export const MONGO_URL = process.env.MONGO_URL;
-export const PORT = 3000;
