cd8fbcca02823b6fef25db223707dbd43c67137d
Daria <dashiko.tyn@gmail.com>
2024-04-18 23:11:45 +0300

    update actors's files

--- actors/routes.js
+++ actors/routes.js
@@ -4,5 +4,11 @@ import actorController from './controller.js';
 const actorsRouter = express.Router();
 
 actorsRouter.get('/', actorController.getActors);
+actorsRouter.get('/single/:id', actorController.getActorById);
+//actorsRouter.get('/movie/:actorId', actorController.getActorsByMovieId);
+actorsRouter.post('/', actorController.createActor);
+actorsRouter.put('/single/:id', actorController.updateActor);
+actorsRouter.delete('/single/:id', actorController.deleteActor);
+
 
 export default actorsRouter;
