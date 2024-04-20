5a1cadc3a2219568987d6823710999cbc47f5395
AlexeyT74 <alexey.tal@gmail.com>
2024-04-20 18:54:17 +0300

    DeleteActor validation added to control incluzion of an actor into movies

--- actors/joi/actors-schema.js
+++ actors/joi/actors-schema.js
@@ -1,4 +1,13 @@
 import Joi from 'joi';
+import { fetchMoviesByActorId } from '../../movies/service.js';
+import logger from '../../utils/logger.js';
+
+// Middleware to validate the actor is not included into movies and it is possible to delete it
+const actorNotInMovies = async (actorId, helpers) => {
+  const movies = await fetchMoviesByActorId(actorId);
+  if (movies && movies.length) return helpers.error('ActorUsed', { message: 'Actor ID is included into some movies' });
+  return actorId;
+};
 
 export const createActorSchema = Joi.object({
   firstName: Joi.string().min(1).max(100).required(),
@@ -11,3 +20,7 @@ export const updateActorSchema = Joi.object({
   firstName: Joi.string().min(1).max(100).optional(),
   lastName: Joi.string().min(1).max(100).optional(),
 }).min(1);
+
+export const deleteActorSchema = Joi.object({
+  id: Joi.string().required().external(actorNotInMovies, 'Validation for actors included in movies'),
+});
